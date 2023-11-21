c667ef5941f35720123a9077c6c4f5db0e438e52
sabbaggh <137013454+sabbaggh@users.noreply.github.com>
2023-11-21 01:10:29 -0600

    Add files via upload

---
+++ practica5.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import numpy as np
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+import random
+
+#Cargar dataset
+df = pd.read_csv('6 class csv2.csv')
+#Revolver las instancias del dataset
+df = df.sample(frac=1)
+#Numero de folds
+k = 4
+#se pasa a una matriz el dataset
+datos = df.to_numpy()
+tam = len(datos)
+#Se calcula el tamanio de cada fold
+tamFolds = int(tam/k)
+#Se define X
+X = datos[:,0:4]
+#Se define Y
+Y = datos[:,4]
+Y = Y.astype(int)
+scaler = StandardScaler()
+#Variables para calcular el error total
+errorSVM = 0
+#Se hace una SVM con un kernel lineal y con uno contra todos
+maquinaSoporte = svm.SVC(kernel='linear', decision_function_shape='ovr')
+
+########################Definir funciones para el algoritmo genetico
+#######################
+######################
+####################
+####################
+individuos = 10
+caracteristicas = len(X[0])
+#Se crea la poblacion inicial con numeros random 0 o 1
+#Se le ponen cinco columnas ya que en la ultima se almacenara la precision y de esta manera sera mas facil manejar los datos
+poblacionInicial = np.random.randint(2,size=(individuos,caracteristicas+1))
+poblacionInicial = poblacionInicial.astype(float)
+#print(poblacionInicial)
+
+
+def evaluacion(poblacion):
+    precisiones = np.zeros((individuos,k))
+    for j in range(individuos):
+        for i in range(k):
+            Xprueba = X[i * tamFolds:(i + 1) * tamFolds, :]
+            Xentrenamiento = np.delete(X, slice(i * tamFolds, (i + 1) * tamFolds), 0)
+            Xentrenamiento = scaler.fit_transform(Xentrenamiento)
+            Xprueba = scaler.transform(Xprueba)
+            Xprueba2 = crearXprueba(Xprueba, poblacion, j)
+            Yprueba = Y[i * tamFolds:(i + 1) * tamFolds]
+            Yentrenamiento = np.delete(Y, slice(i * tamFolds, (i + 1) * tamFolds), 0)
+            maquinaSoporte.fit(Xentrenamiento, Yentrenamiento)
+            precisionSVM = maquinaSoporte.score(Xprueba2, Yprueba)
+            precisiones[j,i] = precisionSVM
+    for i in range(individuos):
+        poblacion[i,4] = np.mean(precisiones[i,:])
+    indices_ordenados = np.argsort(poblacion[:, 4])[::-1]
+    poblacion = poblacion[indices_ordenados]
+    print()
+    print(poblacion)
+    return poblacion
+
+def crearXprueba(Xprueba,poblacion,individuo):
+    for i in range(caracteristicas):
+        if poblacion[individuo,i] == 0:
+            Xprueba[:,i] = 0
+    return Xprueba
+
+def cruza(poblacion):
+    longitud = len(poblacion)
+    hijos = np.zeros((0, caracteristicas+1))
+    for i in range(int(longitud/2)):
+        padre1 = poblacion[i, :]
+        padre2 = poblacion[(longitud-1)-i, :]
+        puntoCruce = random.randint(0, caracteristicas-2)
+        hijo1 = np.zeros((1, caracteristicas+1))
+        hijo2 = np.zeros((1, caracteristicas+1))
+        hijo1[0, :puntoCruce+1] = padre1[:puntoCruce+1]
+        hijo1[0, puntoCruce+1:] = padre2[puntoCruce+1:]
+        hijo2[0, :puntoCruce+1] = padre2[:puntoCruce+1]
+        hijo2[0, puntoCruce+1:] = padre1[puntoCruce+1:]
+        hijos = np.vstack([hijos, hijo1, hijo2])
+    #Se resetean las precisiones a 0
+    hijos[:,4] = 0
+    print(hijos)
+    return hijos
+
+poblacionEvaluada = evaluacion(poblacionInicial)
+cruza(poblacionEvaluada)
+
+#se hace un ciclo for para el numero de folds
+'''for i in range(k):
+    #Se definen los valores de prueba y de entrenamiento y se normalizan
+    Xprueba = X[i*tamFolds:(i+1)*tamFolds,:]
+    Xentrenamiento = np.delete(X,slice(i*tamFolds,(i+1)*tamFolds),0)
+    Xentrenamiento = scaler.fit_transform(Xentrenamiento)
+    Xprueba = scaler.transform(Xprueba)
+    Yprueba = Y[i*tamFolds:(i+1)*tamFolds]
+    Yentrenamiento = np.delete(Y, slice(i*tamFolds,(i+1)*tamFolds),0)
+    #se ajusta el modelo de SVM a nuestros valores de entrenamiento
+    maquinaSoporte.fit(Xentrenamiento,Yentrenamiento)
+    #Se calcula la precision de cada modelo
+    precisionSVM = maquinaSoporte.score(Xprueba,Yprueba)
+    precisionRegLog = regLog.score(Xprueba, Yprueba)
+    precisionBayes = bayes.score(Xprueba, Yprueba)
+    precisionRed = redNeuronal.score(Xprueba,Yprueba)
+    #Se actualiza el error total
+    errorSVM = errorSVM + 1 - precisionSVM
+    errorregLog = errorregLog + 1 - precisionRegLog
+    errorBayes = errorBayes + 1 - precisionBayes
+    errorRed = errorRed + 1 - precisionRed
+    #Se imprime la precision de cada modelo en cada fold
+    print(f'Precision de SVM en el fold {i+1} {precisionSVM*100}%')
+    print(f'Precision de Regresion Logistica en el fold {i+1} {precisionRegLog*100}%')
+    print(f'Precision de Bayes en el fold {i+1} {precisionBayes*100}%')
+    print(f'Precision de la Red Neuronal en el fold {i + 1} {precisionRed * 100}%')
+    print()
+
+errorPromSVM = (errorSVM/k)*100
+
+print(f'Error promedio en SVM {errorPromSVM}%')'''
